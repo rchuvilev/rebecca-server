e52a824eca17b8aaada5749d335d63564ab8b6db
roman.chuvilev <roman.chuvilev@arkadium.com>
2025-01-22 15:07:42 +0400

    123

--- index.ts
+++ index.ts
@@ -1,10 +1,8 @@
-const requestIp = require('request-ip');
-
 Bun.serve({
 	port: 8000,
 	async fetch(req: Request) {
 		const url = new URL(req.url);
-		// Extract relevant data from the request
+		const requestIp = require('request-ip');
 		const requestData = {
 			ip: requestIp.getClientIp(req),
 			url: req.url,
@@ -12,9 +10,17 @@ Bun.serve({
 			headers: req.headers,
 			body: req.method === 'GET' ? null : await req.json(), // Parse body for non-GET requests
 		};
-		// Return the extracted data as a JSON response
-		return new Response(JSON.stringify(requestData, null, 2), {
-			headers: { 'Content-Type': 'application/json' },
-		});
+		switch (url.pathname) {
+			case '/':
+				return new Response('Hello World!', {
+					headers: { 'Content-Type': 'text/plain' },
+				});
+				break;
+			default:
+				return new Response(JSON.stringify(requestData, null, 2), {
+					headers: { 'Content-Type': 'application/json' },
+				});
+				break;
+		}
 	},
 });
