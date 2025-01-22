afbf326bf5fd79b6ac70d6f1f92a152bc5058e2a
roman.chuvilev <roman.chuvilev@arkadium.com>
2025-01-22 13:14:58 +0400

    ESLint and Prettier setup

--- index.ts
+++ index.ts
@@ -1,1 +1,10 @@
-console.log("Hello via Bun!");
+Bun.serve({
+	port: 8000,
+	fetch(req: Request) {
+		const url = new URL(req.url);
+		if (url.pathname === '/') {
+			return new Response('Hello World!');
+		}
+		return new Response('Not Found', { status: 404 });
+	},
+});
